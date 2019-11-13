593e0a07b68a654bc13e7c06808f48c5393eaa42
Carlos <carlos.dom13@hotmail.com>
2019-11-13 01:08:07 +0100

    Updates

---
+++ precache-manifest.8e873a343ed7391bbb60dbcfd0d36153.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640d4cdb15e589d0e4eccc7236b7fd92",
+    "url": "/ejercicio-react-monstruos/index.html"
+  },
+  {
+    "revision": "90b072fcf99167d41a17",
+    "url": "/ejercicio-react-monstruos/static/css/main.db8892ff.chunk.css"
+  },
+  {
+    "revision": "6cff8969416935c332ea",
+    "url": "/ejercicio-react-monstruos/static/js/2.87136607.chunk.js"
+  },
+  {
+    "revision": "90b072fcf99167d41a17",
+    "url": "/ejercicio-react-monstruos/static/js/main.6b34e2fa.chunk.js"
+  },
+  {
+    "revision": "55f39def9c996e604635",
+    "url": "/ejercicio-react-monstruos/static/js/runtime-main.6074fc60.js"
+  }
+]);
